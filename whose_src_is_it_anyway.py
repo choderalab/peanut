47e084f837cccfd9e50ed2d68b7f0cee870429d0
Julie Behr <julie.behr@choderalab.org>
2015-03-03 22:38:50 -0500

    identifying Src

---
+++ whose_src_is_it_anyway.py
@@ -0,0 +1,15 @@
+with open("4_Src_orf_TRANSLATED.txt",'r') as fi:
+    src4 = fi.readline()[:-1]
+
+with open("Human_SRC.txt",'r') as fi:
+    human = fi.readline()[:-1]
+
+with open("Chicken_SRC.txt",'r') as fi:
+    chicken = fi.readline()[:-1]
+
+if src4.find(human) != -1:
+    print("Human Src!")
+elif src4.find(chicken) != -1:
+    print("Chicken Src!")
+else:
+    print("Messed up Src?")
