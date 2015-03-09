8248bab4258bb9f3ad4fca8bf375a1a562278198
Julie Behr <julie.behr@choderalab.org>
2015-03-09 16:27:39 -0400

    Calculating MW correctly

--- convert_dna_to_aa.py
+++ convert_dna_to_aa.py
@@ -90,8 +90,10 @@ def calculate_molecular_weight(aa_sequence):
     # wtf do you do with B and Z?
     aa_to_mw = {'A':89.0935, 'C':121.1590, 'D':133.1032, 'E':147.1299, 'F':165.1900, 'G':75.0669, 'H':155.1552, 'I':131.1736, 'K':146.1882, 'L':131.1736, 'M':149.2124, 'N':132.1184, 'P':115.1310, 'Q':146.1451, 'R':174.2017, 'S':105.0930, 'T':119.1197, 'V':117.1469, 'W':204.2262, 'Y':181.1894}
     molecular_weight = 0.0
-    for char in aa_sequence:
+    for k, char in enumerate(aa_sequence):
         molecular_weight += aa_to_mw[char]
+        if k != 0: # water mlc given off in peptide bond
+            molecular_weight -= 18.01528
     return molecular_weight
 
 def calculate_absorbance(aa_sequence):
