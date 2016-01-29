fa1b04b7e197ecf052f88952ebf9c2a8f17b58c4
Julie Behr <julie.behr@choderalab.org>
2016-01-29 14:48:45 -0500

    documentation

--- peanut/calculate_protein_constants.py
+++ peanut/calculate_protein_constants.py
@@ -1,10 +1,27 @@
 """
-"""
+Calculates extinction coefficients, molecular weight, and absorbance for proteins or 
+protein segments of interest based on string representations of sequences
 
+Dependencies : None
+"""
 
 def calculate_extinction_coeff(aa_sequence, verbose=True):
     """
-    Extinction Coefficient in cm^-1 M^-1
+    Calculates the two extinction coefficients (with and without incorporating Cys) for
+    a given amino acid sequence
+
+    Arguments:
+    ----------
+        aa_sequence : str
+            1-letter code amino acid sequence of protein or protein segment of interest
+        verbose : Bool
+            if verbose==True, print number of Tyr, Trp, and Cys found in sequence
+    Returns:
+    --------
+        secreted : float
+            Extinction Coefficient in cm^-1 M^-1 assuming all cysteines form cystines
+        cytosolic : float
+            Extinction Coefficient in cm^-1 M^-1 ignoring cystines
     """
     count_Tyr = aa_sequence.count('Y')
     count_Trp = aa_sequence.count('W')
@@ -14,13 +31,24 @@ def calculate_extinction_coeff(aa_sequence, verbose=True):
         print(str(count_Trp)+" Trp")
         print(str(count_Cys)+" Cys")
 
-    cytosolic = 1490*count_Tyr + 5500*count_Trp
-    secreted = 1490*count_Tyr + 5500*count_Trp + 125*count_Cys/2
+    cytosolic = 1490.0*count_Tyr + 5500.0*count_Trp
+    secreted = 1490.0*count_Tyr + 5500.0*count_Trp + 125.0*count_Cys/2.0
     return secreted, cytosolic
 
 def calculate_molecular_weight(aa_sequence):
     """
-    Molecular Weight in g/mol
+    Calculates molecular weight of a protein based on a given amino acid sequence
+    Uses dictionary of molecular weight of amino acids and assumes protein weight
+    is the sum of all component amino acids minus a water molecule per peptide bond
+
+    Arguments:
+    ----------
+        aa_sequence : str
+            1-letter code amino acid sequence of protein or protein segment of interest
+    Returns:
+    --------
+        molecular_weight : float 
+            Molecular Weight in g/mol
     """
     # wtf do you do with B and Z?
     aa_to_mw = {'A':89.0935, 'C':121.1590, 'D':133.1032, 'E':147.1299, 'F':165.1900, 'G':75.0669, 'H':155.1552, 'I':131.1736, 'K':146.1882, 'L':131.1736, 'M':149.2124, 'N':132.1184, 'P':115.1310, 'Q':146.1451, 'R':174.2017, 'S':105.0930, 'T':119.1197, 'V':117.1469, 'W':204.2262, 'Y':181.1894}
@@ -39,6 +67,24 @@ def calculate_molecular_weight(aa_sequence):
     return molecular_weight
 
 def calculate_absorbance(aa_sequence, secreted=None, molecular_weight=None):
+    """
+    Calculates absorbance, based on secreted extinction coefficients and molecular weight
+    Will calculate extinction coefficient and / or molecular weight if not given as inputs
+    
+    Arguments:
+    ----------
+        aa_sequence : str
+            1-letter code amino acid sequence of protein or protein segment of interest
+        Optional:
+        ---------
+            secreted : float
+                Extinction Coefficient in cm^-1 M^-1 assuming all cysteines form cystines
+            molecular_weight : float
+                Molecular Weight in g/mol
+    Returns:
+    --------
+        absorbance : float
+    """
     if secreted==None:
         secreted, cytosolic = calculate_extinction_coeff(aa_sequence, verbose=False)
     if molecular_weight==None:
--- peanut/primer_design.py
+++ peanut/primer_design.py
@@ -1,12 +1,66 @@
+"""
+Defines class PrimerGenerator which saves a DNA sequence and translates back and
+forth to amino acid sequences, with methods to generate forward and reverse primers
+for desired point mutations to the wild type.
+
+Dependencies:
+    Requires scikit-bio
+        https://github.com/biocore/scikit-bio
+"""
+
 from skbio.sequence import genetic_code
 from skbio.sequence import DNASequence
 
 class PrimerGenerator(object):
     """
-    Requires scikit-bio
-    https://github.com/biocore/scikit-bio
+    Constructor:
+    ------------
+        PrimerGenerator(sequence, first_res=1)
+        Arguments:
+        ----------
+            sequence : str
+                original DNA nucleotide sequence, treated as wild type
+            first_res : int
+                residue id number of first residue in sequence (default = 1)
+
+    Attributes:
+    -----------
+        self.sequence : string
+            original DNA nucleotide sequence, input at instantiation
+            treated as wild type
+        self.aa_sequence : string
+            wild type sequence of one-letter amino acid codes translated
+            from input nucleotide sequence
+        self.first_res : int
+            index of the first residue in the input sequence
+        self.orig_code : skbio.sequence.genetic_code
+            arbitrary code object used by sci-kit bio as translator
+    Methods:
+    --------
+        make_single_mutant()
+            Determines how many nucleotide changes are required for the desired amino acid
+            mutation, then constructs a primer with a minimum of 25 nucleotides, increasing 
+            the length symmetrically (such that the mutant codon is centered in the primer)
+            up to 45 nucleotides, using the minimum length possible to achieve acceptable
+            melting temperature (78C minimum)
+            Calls sub-methods:
+            ------------------
+                _check_melting_temp()
+                    Calculates melting temp of a given primer sequence, suggests new start and
+                    end indices for primer if melting temp and / or GC content is too low
+                _make_mutant()
+                    Finds the mutant codon, if mutation requires more than 1 nucleotide change
+
     """
     def __init__(self, sequence, first_res=1):
+        """
+        Arguments:
+        ----------
+            sequence : str
+                original DNA nucleotide sequence, treated as wild type
+            first_res : int
+                residue id number of first residue in sequence (default = 1)
+        """
         orig_code = genetic_code(11)
         sequence = sequence.upper()
         aa_sequence = orig_code.translate(sequence).sequence
@@ -17,7 +71,35 @@ class PrimerGenerator(object):
         self.orig_code = orig_code
         return
 
-    def check_melting_temp(self, primer_sequence, start_ix, end_ix, length_sequence):
+    def _check_melting_temp(self, primer_sequence, start_ix, end_ix, length_sequence):
+        """
+        Calculates melting temp of a given primer sequence, suggests new start and
+        end indices for primer if melting temp and / or GC content is too low
+
+        Called iteratively by make_single_mutant()
+
+        Arguments:
+        ----------
+            primer_sequence : str
+                nucleotide sequence including mutant codon to be checked
+            start_ix : int
+                index of the first nucleotide in the primer sequence in the full
+                nucleotide sequence
+            end_ix : int
+                index of the final nucleotide in the primer sequence in the full
+                nucleotide sequence
+            length_sequence : int
+                length of the full nucleotide sequence
+        
+        Returns:
+        --------
+            good_melting_temp : Bool
+                True if primer meets required criteria for GC content and melting temp
+            start_ix : int
+                proposed index for first nucleotide in primer sequence
+            end_ix : int
+                proposed index for final nucleotide in primer sequence
+        """
         N = len(primer_sequence)
         gc_percent = float(primer_sequence.count('g') + primer_sequence.count('c')) / N * 100.0
         mismatch_percent = 1.000 / N * 100.0
@@ -44,17 +126,36 @@ class PrimerGenerator(object):
 
     def make_single_mutant(self, wt_res,res_num,mut_res):
         """
-        sequence (string) DNA sequence
-        wt_res (char) single letter amino acid code of wildtype residue to be mutated
-        res_num (int) residue id number of residue to be mutated
-        mut_res (char) single letter amino acid code of mutant residue
-        first_res (int) residue id number of first residue in sequence (default = 1)
+        Determines how many nucleotide changes are required for the desired amino acid
+        mutation, then constructs a primer with a minimum of 25 nucleotides, increasing 
+        the length symmetrically (such that the mutant codon is centered in the primer)
+        up to 45 nucleotides, using the minimum length possible to achieve acceptable
+        melting temperature (78C minimum)
 
         DNA sequence needs to start with the first residue of the protein (no promoter, etc)
-        take DNA sequence, convert to AA, define AA point mutant, find corresponding codon of wt and mut, output forward and reverse primers
+        take DNA sequence, convert to AA, define AA point mutant, find corresponding codon 
+        of wt and mut, output forward and reverse primers
         DNA sequence should be only the kinase domain
 
-        Desired mutation must require only a single nucleotide change
+        Desired mutation should require only a single nucleotide change; will print warning
+        if more nucleotide changes are required
+
+        Arguments:
+        ----------
+            sequence : str
+                DNA sequence
+            wt_res : char
+                single letter amino acid code of wildtype residue to be mutated
+            res_num : int 
+                residue id number of residue to be mutated
+            mut_res : char
+                single letter amino acid code of mutant residue
+        Returns:
+        --------
+            forward_primer : str
+                nucleotide sequence
+            reverse_primer : str
+                nucleotide sequence
         """
         aa_sequence = self.aa_sequence
         sequence = self.sequence
@@ -75,7 +176,7 @@ class PrimerGenerator(object):
 
         if not mut_codon:
             print("Cannot make desired mutant with a single base change")
-            mut_codon = self.make_mutant(wt_codon, mut_codons)
+            mut_codon = self._make_mutant(wt_codon, mut_codons)
 
         good_melting_temp = False
         start_ix = max(0,(res_num-first_res)*3-11)
@@ -87,7 +188,7 @@ class PrimerGenerator(object):
                 break
             forward_primer = sequence[start_ix:(res_num - first_res)*3]+mut_codon+sequence[(res_num+1 - first_res)*3:end_ix]
             forward_primer = forward_primer.lower()
-            good_melting_temp, start_ix, end_ix = self.check_melting_temp(forward_primer, start_ix, end_ix, len(sequence))
+            good_melting_temp, start_ix, end_ix = self._check_melting_temp(forward_primer, start_ix, end_ix, len(sequence))
     
         forward_sequence = DNASequence(forward_primer)
         reverse_sequence = forward_sequence.rc()
@@ -96,10 +197,23 @@ class PrimerGenerator(object):
 
         return forward_primer, reverse_primer
 
-    def make_mutant(wt_codon, mut_codons):
+    def _make_mutant(wt_codon, mut_codons):
         """
-        """
+        Finds the mutant codon, if mutation requires more than 1 nucleotide change
 
+        Arguments:
+        ----------
+            wt_codon : str
+                len(wt_codon) = 3
+                nucleotide codon from the wild type sequence for the residue to be mutated
+            mut_codons : list(str)
+                all codons that translate to desired mutant residue
+        Returns:
+        --------
+            mut_codon : str
+                codon selected from mut_codons which requires the fewest changes from
+                the wild type codon
+        """
         mut_codons = [DNASequence(codon) for codon in mut_codons]
         distances = [wt_codon.distance(codon) for codon in mut_codons]
 
